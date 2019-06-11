6510110f6cda144880241fa109e30ea87f1e9d1b
appleternity <b99901034@ntu.edu.tw>
2019-06-11 16:09:28 -0400

    homepage

--- static/main.js
+++ static/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
     console.log("Hello");
 
+    $(document).on("click", "paper", function(evt) {
+        window.open($(evt.target).attr("href"), '_blank');
+    });
+
+    
+    $(document).on("click", "publisher", function(evt) {
+        window.open($(evt.target).attr("href"), '_blank');
+    });
 
 });
